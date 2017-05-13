13d85393043a8ad92e725739540d4971977e7c9b
Roman <bondrogeen@gmail.com>
2017-05-13 16:54:37 +0300

    автостарт

--- app/src/main/java/de/fun2code/android/pawserver/ServerReceiver.java
+++ app/src/main/java/de/fun2code/android/pawserver/ServerReceiver.java
@@ -5,23 +5,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-
 public class ServerReceiver extends BroadcastReceiver {
     final String TAG = "ioBroker.paw";
     private Context context;
+    private Intent intent;
     public void onReceive(Context context, Intent intent) {
         this.context = context;
-        Log.i(TAG, "onReceive " + intent.getAction());
-        //context.startService(new Intent(context, ServerActivity.class));
-        Log.i(TAG, "Start iobroker");
+        this.intent = intent;
         try {
             Thread.sleep(10000L);
         } catch (InterruptedException var3) {
             ;
         }
-        Log.i(TAG, "Start + 10сек iobroker");
+        Log.i(TAG, "Autostart ioBroker");
 
-        this.context.startService(new Intent(this.context, ServerActivity.class));
+        Intent i = new Intent(context, ServerActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
     }
 }
 
