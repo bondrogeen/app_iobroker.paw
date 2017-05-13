165353ed1f9c1de8fd07dd4a74dcf11a329c9b44
Roman <bondrogeen@gmail.com>
2017-05-13 21:25:04 +0300

    bag

--- app/src/main/java/de/fun2code/android/pawserver/Device.java
+++ app/src/main/java/de/fun2code/android/pawserver/Device.java
@@ -1,33 +1,119 @@
 package de.fun2code.android.pawserver;
 
-import android.app.AlertDialog;
+
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.io.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import static de.fun2code.android.pawserver.ServerActivity.*;
+
 
 public class Device extends PawServerActivity {
+    public boolean useWakeLock = true;
+    public boolean hideNotificationIcon = false;
+    public boolean execAutostartScripts = false;
+    public boolean showUrlInNotification = false;
+    public boolean restartIpChanged = false;
+    public Context contex;
 
+    String pawHome = INSTALL_DIR + "/";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.device);
-        //Log.i(TAG, "Start SETTINGS "+INSTALL_DIR);
+        Log.i(TAG, "Start SETTINGS "+INSTALL_DIR);
+        Log.i(TAG, "pawHome "+pawHome);
+        //readPreferences();
+        saveSharedPreferences();
+
     }
+
+    private void saveSharedPreferences()
+    {
+        // создадим для примера несколько строчек с настройками. Вы можете пропустить этот код
+        SharedPreferences prefs = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+
+        prefEdit.putBoolean("HideNotificationIcon", false);
+        prefEdit.putBoolean("ExecAutostartScripts", false);
+        prefEdit.putBoolean("UseWakeLock", true);
+        prefEdit.putBoolean("ShowUrlInNotification", false);
+        prefEdit.putString("PawHome", "/");
+        prefEdit.putBoolean("RestartIpChanged", false);
+        //prefEdit.clear().commit();
+        prefEdit.commit();
+
+        // Теперь сам пример
+        File myPath = new File(INSTALL_DIR);
+        File myFile = new File(myPath, "MySharedPreferences");
+        Log.i(TAG, "save "+INSTALL_DIR);
+        try
+        {
+            FileWriter fw = new FileWriter(myFile);
+            PrintWriter pw = new PrintWriter(fw);
+
+            Map<String,?> prefsMap = prefs.getAll();
+
+            for(Map.Entry<String,?> entry : prefsMap.entrySet())
+            {
+                pw.println(entry.getKey() + ": " + entry.getValue().toString());
+            }
+
+            pw.close();
+            fw.close();
+        }
+        catch (Exception e)
+        {
+            // what a terrible failure...
+            Log.i(TAG, e.toString());
+        }
+    }
+
+
+    public void readPreferences() {
+
+
+        SharedPreferences preferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+        hideNotificationIcon = preferences.getBoolean("HideNotificationIcon", false);
+        execAutostartScripts = preferences.getBoolean("ExecAutostartScripts", false);
+        useWakeLock = preferences.getBoolean("UseWakeLock", true);
+        showUrlInNotification = preferences.getBoolean("ShowUrlInNotification", false);
+        pawHome = preferences.getString("PawHome", pawHome);
+        restartIpChanged = preferences.getBoolean("RestartIpChanged", true);
+
+        Log.i(TAG, "hideNotificationIcon "+hideNotificationIcon);
+    }
+
+
+
+    /*
+    this.context = context;
+            SharedPreferences settings = context.getSharedPreferences("PawServer", 0);
+            this.pawHome = settings.getString("PawHome", (String)null);
+            File preferencesFile = new File(this.pawHome, "etc/preferences");
+            if(preferencesFile.isFile() && preferencesFile.exists()) {
+            try {
+            ServerConfigUtil.injectPreferences(context, preferencesFile);
+            } catch (FileNotFoundException var6) {
+            Log.e("PawServerReceiver", "Preference file does not exist!");
+            } catch (IOException var7) {
+            Log.e("PawServerReceiver", "Preference file I/O error!");
+            }
+            }
+
+    */
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
-
-
 
 
 }
--- app/src/main/java/de/fun2code/android/pawserver/ServerService.java
+++ app/src/main/java/de/fun2code/android/pawserver/ServerService.java
@@ -1,5 +1,8 @@
 package de.fun2code.android.pawserver;
 
+import android.content.SharedPreferences;
+import android.util.Log;
+
 /**
  * Sample "Build your own PAW server" Service.
  *
@@ -10,45 +13,36 @@ public class ServerService extends PawServerService {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		/*
-		 * Individual settings.
-		 */
-		init();
-	}
 
-
-	/*
-	 * Service options are:
-	 * TAG = Tag name for message logging.
-	 * startOnBoot = Indicates if service has been started on boot.
-	 * isRuntime = If set to true this will  only allow local connections.
-	 * serverConfig = Path to server configuration directory.
-	 * pawHome = PAW installation directory.
-	 * useWakeLock = Switch wakelock on or off.
-	 * hideNotificationIcon = Set to true if no notifications should be shown.
-	 * execAutostartScripts = Set to true if scripts inside the autostart directory should be executed onstartup.
-	 * showUrlInNotification = Set to true if URL should be shown in notification.
-	 * notificationTitle = The notification title.
-	 * notificationMessage = The notification message.
-	 * appName = Application name"
-	 * activityClass = Activity class name.
-	 * notificationDrawableId = ID of the notification icon to display.
-	 */
-	private void init() {
 		TAG = getString(R.string.app_name);
-		startedOnBoot = true;
-		isRuntime = false;
+		SharedPreferences preferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
+		hideNotificationIcon = preferences.getBoolean("HideNotificationIcon", false);
+		execAutostartScripts = preferences.getBoolean("ExecAutostartScripts", false);
+		useWakeLock = preferences.getBoolean("UseWakeLock", true);
+		showUrlInNotification = preferences.getBoolean("ShowUrlInNotification", false);
+		//pawHome = preferences.getString("PawHome", pawHome);
+		restartIpChanged = preferences.getBoolean("RestartIpChanged", true);
+		startedOnBoot = preferences.getBoolean("startedOnBoot", true);
+		isRuntime = preferences.getBoolean("isRuntime", false);
+		Log.i(TAG, "isRuntime "+isRuntime);
+
+
+		//TAG = getString(R.string.app_name);
+		//startedOnBoot = true;
+		//isRuntime = false;
 		serverConfig = ServerActivity.INSTALL_DIR + "/conf/server.xml";
 		pawHome = ServerActivity.INSTALL_DIR + "/";
-		useWakeLock = true;
-		hideNotificationIcon = false;
-		execAutostartScripts = false;
-		showUrlInNotification = false;
+		//useWakeLock = true;
+		//hideNotificationIcon = false;
+		//execAutostartScripts = false;
+		//showUrlInNotification = false;
+		//restartIpChanged = true;
 		notificationTitle = "Start iobroker.paw";
 		notificationMessage = "Start server";
 		appName = getString(R.string.app_name);
 		activityClass = "de.fun2code.android.pawserver.ServerActivity";
 		notificationDrawableId = R.drawable.ic_launcher;
+
 	}
 
 }
