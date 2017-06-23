a96582793bf3b79bab2c9c31a627e9fd257dc0f7
Roman <bondrogeen@gmail.com>
2017-06-23 21:28:21 +0300

    Sensor

--- app/src/main/java/de/fun2code/android/pawserver/ServerService.java
+++ app/src/main/java/de/fun2code/android/pawserver/ServerService.java
@@ -1,7 +1,23 @@
 package de.fun2code.android.pawserver;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.util.Log;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Sample "Build your own PAW server" Service.
@@ -9,7 +25,12 @@ import android.util.Log;
  *
  */
 public class ServerService extends PawServerService {
-
+	SensorManager sensorManager;
+	private static String server;
+	private static String port;
+	private static String device;
+	private static String namespace;
+	private static String light;
 	@Override
 	public void onCreate() {
 		super.onCreate();
@@ -25,8 +46,6 @@ public class ServerService extends PawServerService {
 		startedOnBoot = preferences.getBoolean("startedOnBoot", true);
 		isRuntime = preferences.getBoolean("isRuntime", false);
 		Log.i(TAG, "isRuntime "+isRuntime);
-
-
 		//TAG = getString(R.string.app_name);
 		//startedOnBoot = true;
 		//isRuntime = false;
@@ -42,7 +61,83 @@ public class ServerService extends PawServerService {
 		appName = getString(R.string.app_name);
 		activityClass = "de.fun2code.android.pawserver.ServerActivity";
 		notificationDrawableId = R.drawable.ic_launcher;
+		sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 
+		List<Sensor> sensors= sensorManager.getSensorList(Sensor.TYPE_ALL);
+		for (Sensor sensor : sensors) {
+			if(sensor.getType()== 5 ){
+				Sensor light = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+				sensorManager.registerListener(workingSensorEventListener, light, SensorManager.SENSOR_DELAY_NORMAL);
+				Log.d("Sensors", "" + sensor.getName());
+			}
+		}
+
+		server = preferences.getString("server", "");
+		port = preferences.getString("port", "");
+		device = preferences.getString("device", "");
+		namespace = preferences.getString("namespace", "");
+	}
+
+	private final SensorEventListener workingSensorEventListener = new SensorEventListener() {
+
+		public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		}
+
+		public void onSensorChanged(SensorEvent event) {
+			Log.i(TAG, "type "+event.sensor.getType());
+			if(event.sensor.getType() == 8) {
+				//this.proximity = event.values[0];
+				Log.i(TAG, "proximity: "+event.values[0]);
+				light = String.valueOf(event.values[0]);
+				new SendSensor().execute();
+			}
+		}
+	};
+
+
+	public void onDestroy() {
+		super.onDestroy();
+		sensorManager.unregisterListener(workingSensorEventListener);
+	}
+
+	class SendSensor extends AsyncTask<String, String, String> {
+		@Override
+		protected String doInBackground(String... params) {
+			String response = null;
+			Log.i(TAG, "Start ");
+			String url = "http://" + server + ":" + port;
+			try {
+				DefaultHttpClient hc = new DefaultHttpClient();
+				ResponseHandler<String> res = new BasicResponseHandler();
+				HttpPost postMethod = new HttpPost(url);
+				List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+				nameValuePairs.add(new BasicNameValuePair("send", "info.sensors"));
+				nameValuePairs.add(new BasicNameValuePair("value", light));
+				nameValuePairs.add(new BasicNameValuePair("device", device));
+				nameValuePairs.add(new BasicNameValuePair("namespace", namespace));
+				postMethod.setEntity(new UrlEncodedFormEntity(nameValuePairs,"UTF-8"));
+				response = hc.execute(postMethod, res);
+			} catch (Exception e) {
+				Log.i(TAG, "err " + e);
+				return String.valueOf(e);
+			}
+			return response;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			Log.i(TAG, "RequestTask " + result);
+			if(result.equals("post received")){
+				//postStatus = true;
+			}else{
+				//postStatus = false;
+			}
+		}
+
+		@Override
+		protected void onPreExecute() {
+
+		}
 	}
 
 }
--- app/src/main/java/de/fun2code/android/pawserver/Speech.java
+++ app/src/main/java/de/fun2code/android/pawserver/Speech.java
@@ -55,7 +55,7 @@ public class Speech extends Activity {
 
         sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
 
-        Sensor light = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        Sensor light = sensorManager.getDefaultSensor(Sensor.TYPE_ALL);
         sensorManager.registerListener(workingSensorEventListener, light, SensorManager.SENSOR_DELAY_NORMAL);
 
         //speech_text.setText("");
@@ -121,14 +121,49 @@ public class Speech extends Activity {
 
     private final SensorEventListener workingSensorEventListener = new SensorEventListener() {
 
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                }
 
-        public void onSensorChanged(SensorEvent event) {
-            // Получаем атмосферное давление в миллибарах
-            double pressure = event.values[0];
+            public void onSensorChanged(SensorEvent event) {
+                Log.i(TAG, "type "+event.sensor.getType());
+                if(event.sensor.getType() == 1 && event.values.length == 3) {
+                    //this.currentTime = System.currentTimeMillis();
+                    //long diffTime = this.currentTime - this.lastUpdate;
+                    //this.lastUpdate = this.currentTime;
+                    //this.dataX = event.values[0];
+                    //this.dataY = event.values[1];
+                    //this.dataZ = event.values[2];
+                    //this.dataForce = Math.abs(this.dataX + this.dataY + this.dataZ - this.last_x - this.last_y - this.last_z) / (float)diffTime * 10000.0F;
+                    //this.last_x = this.dataX;
+                    //this.last_y = this.dataY;
+                    //this.last_z = this.dataZ;
+                } else if(event.sensor.getType() == 3) {
+                    //this.orientAccuracy = event.accuracy;
+                    //this.lastOrientTimestamp = System.currentTimeMillis();
+                    //this.orientBearing = event.values[0];
+                } else if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
+                    //this.brightness = event.values[0];
+                    //Log.i(TAG, "brightness: "+event.values[0]);
+                } else if(event.sensor.getType() == 8) {
+                    //this.proximity = event.values[0];
+                Log.i(TAG, "proximity: "+event.values[0]);
+            } else if(event.sensor.getType() == 6) {
+                //this.pressure = event.values[0];
+            } else if(event.sensor.getType() == 4) {
+                //this.gyroX = event.values[0];
+                //this.gyroY = event.values[1];
+                //this.gyroZ = event.values[2];
+            } else if(event.sensor.getType() == 12) {
+                //this.relativeHumidity = event.values[0];
+            } else if(event.sensor.getType() == 13) {
+                //this.ambientTemperature = event.values[0];
+            } else {
+                if(event.sensor.getType() != 19) {
+                    return;
+                }
 
-            Log.i(TAG, "size ");
+                //this.stepCounter = event.values[0];
+            }
 
         }
     };
